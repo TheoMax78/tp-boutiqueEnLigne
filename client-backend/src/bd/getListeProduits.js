9eca5572948e6a4bf96b9d1cb7241652f7aba12a
Jonathan <joe293@hotmail.com>
2021-07-31 14:56:05 -0400

    Découpage des fonctions + tests

--- client-backend/src/bd/getListeProduits.js
+++ client-backend/src/bd/getListeProduits.js
@@ -7,21 +7,16 @@ export function getListeProduits(requete, reponse) {
     
     let listeProduit = [];
 
-    if (quantiteParPage !== undefined && pageActive !== undefined) { 
-        const decalage = (pageActive - 1) * quantiteParPage;
-        
-        const finPage = decalage + quantiteParPage;       
+    if (quantiteParPage !== undefined && pageActive !== undefined) {
         
         utiliserDB(async (db) => {
 
             listeProduit = await getListeProduitsDB(db);
 
             if (categorie !== undefined && categorie !== "Tous") {
-                const listeProduitFiltrer =  listeProduit.filter(produit => produit.categorie === categorie);
-                listeProduit.unshift(listeProduitFiltrer);
+                const listeProduitFiltrer = FiltrerCategorie(categorie, listeProduit);
             }
-            
-            const listeProduitTrier = listeProduit.slice(decalage, finPage);
+            const listeProduitTrier = SeparerListeParPage(pageActive, listeProduit, quantiteParPage);
             reponse.status(200).json(listeProduitTrier);
             
         }, reponse).catch(
@@ -32,7 +27,67 @@ export function getListeProduits(requete, reponse) {
     else {
         reponse.status(500).send("Paramètre manquant")
     }
-}  
+}
+
+export function ValiderParametresDefinis(p_parametre) {
+
+    if(p_parametre === undefined)  {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+export function CalculerDecalage(p_pageActive, p_quantiteParPage) {
+    if (ValiderParametresDefinis(p_pageActive) === true &&        
+        ValiderParametresDefinis(p_quantiteParPage) === true){
+            
+            if(isFinite(p_pageActive) && isFinite(p_quantiteParPage)) {
+                if(p_pageActive > 0 && p_quantiteParPage > 0) {
+                    const decalage = (p_pageActive - 1) * p_quantiteParPage;
+                    return decalage;
+                }
+                else {
+                    return String("Les paramètres doivent être supérieurs à zéro");
+                }
+                
+            } 
+            else {
+                return String("Les paramètres doivent être des nombres");
+            }
+
+    }
+    else {
+        return String("Certains paramètres ne sont pas définis");
+    }   
+}
+
+export function FiltrerCategorie(p_categorie, p_listeProduit) {
+    const listeProduitFiltrer = p_listeProduit.filter(produit => produit.categorie === p_categorie);
+    p_listeProduit.unshift(listeProduitFiltrer);
+    return p_listeProduit;
+}
+
+export function SeparerListeParPage(p_pageActive, p_listeProduit, p_quantiteParPage) {
+    if (ValiderParametresDefinis(p_pageActive) === true &&
+        ValiderParametresDefinis(p_listeProduit) === true &&
+        ValiderParametresDefinis(p_quantiteParPage) === true) {
+
+            if (isFinite(p_pageActive) && isFinite(p_quantiteParPage)) {
+                const decalage = CalculerDecalage(p_pageActive, p_quantiteParPage);
+                const finPage = decalage + p_quantiteParPage;
+                const listeProduitTrier = p_listeProduit.slice(decalage, finPage);
+                return listeProduitTrier;
+            } 
+            else {
+                return String("Certains paramètres sont incorrects");
+            }  
+    }
+    else {
+        return String("Certains paramètres ne sont pas définis");
+    }
+}
 
 export async function getListeProduitsDB(db) {
     return await db.collection('produits').find({}).toArray();
--- client-backend/src/bd/getListeProduits.test.js
+++ client-backend/src/bd/getListeProduits.test.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 import 'regenerator-runtime/runtime';
 
-import { getListeProduitsDB } from './getListeProduits';
+import { CalculerDecalage, getListeProduitsDB, SeparerListeParPage } from './getListeProduits';
 
 describe('get', () => {
     let connection, db;
@@ -20,70 +20,96 @@ describe('get', () => {
 
         expect(produits.length).toEqual(65);
         expect(produits.length).not.toEqual(40);
+        expect(produits[0].nom).toEqual("Claritin");
+        expect(produits[64].nom).toEqual("Star Wars");
     });
 
-    test('Devrait retourner 12 produits', async () => {
+    test('Devrait retourner 12 produits, page 1', async () => {
         const produits = await getListeProduitsDB(db);
-        const quantiteParPage = 12;
-        const pageActive = 1;
+        
+        const decalage = CalculerDecalage(1, 12);
+        const produitsTrier = SeparerListeParPage(1, produits, 12);
+        
+        expect(produitsTrier.length).toEqual(12);
+        expect(produitsTrier[0].nom).toMatch("Claritin");
+        expect(produitsTrier[11].nom).toMatch(/Aveda/);
+        expect(decalage).toEqual(0);
+        expect(produitsTrier.length).not.toEqual(40);
+    });
 
-        const decalage = (pageActive - 1) * quantiteParPage;
-        const finPage = decalage + quantiteParPage;
-        const produitsTrier = produits.slice(decalage, finPage);
+    test('Devrait retourner 12 produits, page 3', async () => {
+        const produits = await getListeProduitsDB(db);
+        
+        const decalage = CalculerDecalage(3, 12);
+        const produitsTrier = SeparerListeParPage(3, produits, 12);
         
         expect(produitsTrier.length).toEqual(12);
         expect(produitsTrier.length).not.toEqual(40);
+        expect(decalage).toEqual(24);
+        expect(produitsTrier[0].nom).toMatch(/Narnia/);
+        expect(produitsTrier[11].nom).toMatch(/Godfather/);
     });
 
-    test('Devrait retourner 24 produits', async () => {
+    test('Devrait retourner 24 produits, page 1', async () => {
         const produits = await getListeProduitsDB(db);
-        const quantiteParPage = 24;
-        const pageActive = 1;
-
-        const decalage = (pageActive - 1) * quantiteParPage;
-        const finPage = decalage + quantiteParPage;
-        const produitsTrier = produits.slice(decalage, finPage);
-
+        
+        const decalage = CalculerDecalage(1, 24);
+        const produitsTrier = SeparerListeParPage(1, produits, 24);
+        
         expect(produitsTrier.length).toEqual(24);
         expect(produitsTrier.length).not.toEqual(40);
+        expect(produitsTrier[0].nom).toMatch("Claritin");
+        expect(produitsTrier[23].nom).toMatch(/Octa/);
+        expect(decalage).toEqual(0);
+    });
+    
+    test('Devrait retourner 24 produits, page 3', async () => {
+        const produits = await getListeProduitsDB(db);
+        
+        const decalage = CalculerDecalage(3, 24);
+        const produitsTrier = SeparerListeParPage(3, produits, 24);
+        
+        expect(produitsTrier.length).toEqual(17);
+        expect(produitsTrier.length).not.toEqual(40);
+        expect(produitsTrier[0].nom).toMatch("Shutter Island");
+        expect(produitsTrier[16].nom).toMatch(/Star/);
+        expect(decalage).toEqual(48);
     });
 
-    test('Devrait retourner 48 produits', async () => {
+    test('Devrait retourner 48 produits, page 1', async () => {
         const produits = await getListeProduitsDB(db);
-        const quantiteParPage = 48;
-        const pageActive = 1;
 
-        const decalage = (pageActive - 1) * quantiteParPage;
-        const finPage = decalage + quantiteParPage;
-        const produitsTrier = produits.slice(decalage, finPage);
+        const decalage = CalculerDecalage(1, 48);
+        const produitsTrier = SeparerListeParPage(1, produits, 48);
         
         expect(produitsTrier.length).toEqual(48);
+        expect(produitsTrier[0].nom).toMatch("Claritin");
+        expect(produitsTrier[47].nom).toMatch(/Dory/);
         expect(produitsTrier.length).not.toEqual(40);
+        expect(decalage).toEqual(0);
     });
 
-    test('Devrait retourner un tableau vide', async () => {
+    test('Devrait retourner 48 produits, page 2', async () => {
         const produits = await getListeProduitsDB(db);
-        const quantiteParPage = "aaa";
-        const pageActive = 1;
 
-        const decalage = (pageActive - 1) * quantiteParPage;
-        const finPage = decalage + quantiteParPage;
-        const produitsTrier = produits.slice(decalage, finPage);
-
-        expect(produitsTrier.length).toBe(0);        
+        const decalage = CalculerDecalage(2, 48);
+        const produitsTrier = SeparerListeParPage(2, produits, 48);
         
+        expect(produitsTrier.length).toEqual(17);
+        expect(produitsTrier[0].nom).toMatch(/Shut/);       
+        expect(produitsTrier[16].nom).toMatch(/Wars/);
+        expect(produitsTrier.length).not.toEqual(40);
+        expect(decalage).toEqual(48);
     });
 
     test('Devrait retourner un erreur', async () => {
-        const produits = await getListeProduitsDB(db);
-        const quantiteParPage = undefined;
-        const pageActive = 1;
+        const produits = await getListeProduitsDB(db);        
 
-        const decalage = (pageActive - 1) * quantiteParPage;
-        const finPage = decalage + quantiteParPage;
-        const produitsTrier = produits.slice(decalage, finPage);
-        
-        expect(produitsTrier.length).toBe(0);
-        expect(quantiteParPage).toBeUndefined();
+        expect(CalculerDecalage(0, 48)).toMatch(/supérieur/);
+        expect(CalculerDecalage(48)).toMatch(/définis/);         
+        expect(SeparerListeParPage(1, produits, "aaa")).toMatch(/incorrect/);
+        expect(SeparerListeParPage()).toMatch(/pas définis/);
+        expect(SeparerListeParPage(1, "")).toMatch(/pas définis/);
+        expect(SeparerListeParPage(produits, 48)).toMatch(/pas définis/);
     });
 });
